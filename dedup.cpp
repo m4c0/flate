9bd5e5015da4a59ed98c88ed012638a5860c2fb4
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-01 19:49:51 +0100

    dedup: respect len between 3/258

--- dedup.cpp
+++ dedup.cpp
@@ -2,7 +2,8 @@ module flate;
 
 namespace flate {
 constexpr symbols::symbol dedup(const uint8_t *data, int len, int &ptr) {
-  static constexpr const auto wnd_size = 32000;
+  static constexpr const auto max_dist = 32768;
+  static constexpr const auto max_len = 258;
 
   if (ptr < 0 || ptr >= len)
     return {type::end};
@@ -10,20 +11,22 @@ constexpr symbols::symbol dedup(const uint8_t *data, int len, int &ptr) {
   int l_len = 0;
   int l_dist = 0;
 
-  for (auto i = ptr - 1; i >= 0 && (ptr - i < wnd_size); i--) {
+  for (auto i = ptr - 1; i >= 0 && (ptr - i < max_dist); i--) {
     if (data[i] != data[ptr])
       continue;
 
     int j{};
-    for (; ptr + j < len; j++) {
+    for (; ptr + j < len && j < max_len; j++) {
       if (data[i + j] != data[ptr + j])
         break;
     }
-    l_len = j;
-    l_dist = ptr - i;
+    if (j > l_len) {
+      l_len = j;
+      l_dist = ptr - i;
+    }
   }
 
-  if (l_len <= 1)
+  if (l_len <= 2) // min_dist = 3
     return {type::raw, 0, 0, data[ptr++]};
 
   ptr += l_len;
@@ -59,6 +62,28 @@ static_assert([] {
   assert_next({type::raw, 0, 0, 'O'});
   assert_next({type::repeat, 3, 5});
   assert_next({type::raw, 0, 0, '\0'});
+  assert_next({type::end});
+  return true;
+}());
+static_assert([] {
+  constexpr const uint8_t data[300]{};
+
+  int ptr{};
+  const auto assert_next = [&](symbol expected) {
+    auto sym = dedup(data, sizeof(data), ptr);
+    if (sym.type != expected.type)
+      throw 0;
+    if (sym.len != expected.len)
+      throw 0;
+    if (sym.dist != expected.dist)
+      throw 0;
+    if (sym.c != expected.c)
+      throw 0;
+  };
+
+  assert_next({type::raw, 0, 0, '\0'});
+  assert_next({type::repeat, 258, 1});
+  assert_next({type::repeat, 41, 1});
   assert_next({type::end});
   return true;
 }());
