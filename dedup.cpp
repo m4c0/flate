693fafccc0be8e343b211e33c78e142206349dbf
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-01 19:24:47 +0100

    dedup: support for emitting "repeat"

--- dedup.cpp
+++ dedup.cpp
@@ -1,19 +1,41 @@
 module flate;
 
 namespace flate {
-constexpr symbols::symbol dedup(const uint8_t *data, unsigned len,
-                                unsigned &ptr) {
+constexpr symbols::symbol dedup(const uint8_t *data, int len, int &ptr) {
+  static constexpr const auto wnd_size = 32000;
+
   if (ptr < 0 || ptr >= len)
     return {type::end};
 
-  return {type::raw, 0, 0, data[ptr++]};
+  int l_len = 0;
+  int l_dist = 0;
+
+  for (auto i = ptr - 1; i >= 0 && (ptr - i < wnd_size); i--) {
+    if (data[i] != data[ptr])
+      continue;
+
+    int j{};
+    for (j = 1; ptr + j < len; j++) {
+      if (data[i + j] != data[ptr + j])
+        break;
+    }
+    l_len = j;
+    l_dist = ptr - i;
+  }
+
+  if (l_len <= 1)
+    return {type::raw, 0, 0, data[ptr++]};
+
+  ptr += l_len - l_dist;
+  return {type::repeat, static_cast<unsigned>(l_len),
+          static_cast<unsigned>(l_dist)};
 }
 } // namespace flate
 
 static_assert([] {
   constexpr const uint8_t data[]{"HEYHEYHEY YO YO YO"};
 
-  unsigned ptr{};
+  int ptr{};
   const auto assert_next = [&](symbol expected) {
     auto sym = dedup(data, sizeof(data), ptr);
     if (sym.type != expected.type)
@@ -29,6 +51,7 @@ static_assert([] {
   assert_next({type::raw, 0, 0, 'H'});
   assert_next({type::raw, 0, 0, 'E'});
   assert_next({type::raw, 0, 0, 'Y'});
+  assert_next({type::repeat, 6, 3});
 
   // assert_next({type::end});
   return true;
