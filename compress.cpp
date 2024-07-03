b06840aec2df30a3d835cc4bc0b2fe183f185c1f
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-03 18:34:51 +0100

    compress: implementation

--- compress.cpp
+++ compress.cpp
@@ -5,27 +5,44 @@ import hai;
 static constexpr auto compress(yoyo::writer &w, const uint8_t *data,
                                unsigned size) {
   bitwriter bitw{size};
+  bitw.write(1, 1);
+  bitw.write(1, 2);
 
   auto syms = dedup_all(data, size);
   static_huffman_encode(syms, bitw);
+  bitw.flush();
 
-  if (bitw.buffer().size() == size)
-    // write using no-comp
-    return mno::req<void>{};
+  auto &buf = bitw.buffer();
+  if (buf.size() != size)
+    return w.write(buf.begin(), buf.size());
 
-  // write using comp
-  return mno::req<void>{};
+  // write using no-comp
+  return mno::req<void>::failed("TODO: implement uncompressed case");
 }
 
 static_assert([] {
-  constexpr const uint8_t data[]{"HUEHUEHUE"};
+  constexpr const uint8_t data[]{"HUEHUEHUEHUE"};
+  // H(72),   U(85),   E(69),   rep(9,3)[263], 0,       end
+  // 01111000 10000101 01110101 0000111 00010  00110000 0000000
 
-  hai::array<uint8_t> buf{6};
+  hai::array<uint8_t> buf{16};
   yoyo::memwriter w{buf};
 
   if (!::compress(w, data, sizeof(data)).is_valid())
     throw 0;
 
+  constexpr const auto assert = [](auto res, auto exp) {
+    if (res != exp)
+      throw 0;
+  };
+  assert(buf[0], 0b11000011);
+  assert(buf[1], 0b00101011);
+  assert(buf[2], 0b10101100);
+  assert(buf[3], 0b00111011);
+  assert(buf[4], 0b00001000);
+  assert(buf[5], 0b00011000);
+  assert(buf[6], 0b00000000);
+  assert(w.raw_pos(), 7);
   return true;
 }());
 
