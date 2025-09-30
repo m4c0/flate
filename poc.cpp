f4c56309ac12f2a86aa621eef2e8dbda87fe2390
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-30 11:19:50 +0100

    poc: cleaning up example

--- poc.cpp
+++ poc.cpp
@@ -2,26 +2,13 @@
 
 import flate;
 import hai;
-import silog;
+import print;
 import traits;
 import yoyo;
 
 using namespace traits::ints;
 
-#if 0
-constexpr const uint8_t lorem_ipsum[] = R"(
-Ut et praesentium perspiciatis hic. Doloribus ut architecto non et eligendi. Hic aliquid ea corporis.
-
-Ipsa asperiores architecto ad facere et. Vel et temporibus rerum beatae corporis. Eius odit et tenetur vero et et necessitatibus. Totam error dicta non in atque dolor omnis ab. Unde molestiae repellendus at qui est.
-
-Facilis cum ea ullam qui et et fugit. Est atque non repellendus similique fugiat eaque adipisci. Ut inventore sequi vero voluptatem quibusdam aut nesciunt. Nostrum culpa non ipsam natus molestias.
-
-Numquam iste est consequatur optio voluptatibus nihil. Tenetur non ipsa id. Quia consequatur doloribus et in occaecati maxime voluptate. Consectetur tempora laudantium non. Eos autem porro ut aliquam consequuntur.
-
-Illum voluptatibus aut esse. Omnis asperiores praesentium quo voluptas quia quidem ut dolores. Ut quod nihil illo consequatur aut.
-)";
-#else
-// Brainf**k quine, should compress more
+// Brainf**k quine, should compress nicely
 constexpr const uint8_t lorem_ipsum[] =
     "->+>+++>>+>++>+>+++>>+>++>>>+>+>+>++>+>>>>+++>+>>++>+>+++>>++>++>>+>>+>++>"
     "++>+>>>>+++>+>>>>++>++>>>>+>>++>+>+++>>>++>>++++++>>+>>++>+>>>>+++>>+++++>"
@@ -35,11 +22,9 @@ constexpr const uint8_t lorem_ipsum[] =
     ">+++++++++<]>-.------------------->-[-<.<+>>]<[+]<+>>>]<<<[-[-[-[>>+<+++++"
     "+[->+++++<]]>++++++++++++++<]>+++<]++++++[->+++++++<]>+<<<-[->>>++<<<]>[->"
     ">.<<]<<]";
-#endif
 
 int main() {
-  silog::log(silog::info, "Original string has %d bytes",
-             static_cast<int>(sizeof(lorem_ipsum)));
+  putfn("Original string has %d bytes", static_cast<int>(sizeof(lorem_ipsum)));
 
   hai::array<uint8_t> comp{10240};
   yoyo::memwriter wr{comp};
@@ -50,17 +35,15 @@ int main() {
   hai::array<uint8_t> decomp{10240};
 
   return flate::compress(wr, lorem_ipsum, sizeof(lorem_ipsum))
-      .map([&] {
-        silog::log(silog::info, "Compressed to %d bytes", wr.raw_pos());
-      })
+      .map([&] { putfn("Compressed to %d bytes", wr.raw_pos()); })
       .fmap([&] { return flate::huffman_reader::create(&bs); })
       .fmap([&](auto &hr) {
         return hr.read_up_to(decomp.begin(), decomp.size());
       })
-      .map([](auto n) { silog::log(silog::info, "Got %d bytes back", n); })
+      .map([](auto n) { putfn("Got %d bytes back", n); })
       .map([] { return 0; })
       .log_error([&] {
-        silog::log(silog::info, "Total of %d bytes read", rd.raw_pos());
+        putfn("Total of %d bytes read before error", rd.raw_pos());
         return 1;
       });
 }
